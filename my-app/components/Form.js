6c552ac04a674d67cb8a34d824f9b99edf1a5ae6
Kc pele <fidekg123@gmail.com>
2022-06-22 09:07:36 +0000

    irst commit

---
+++ my-app/components/Form.js
@@ -0,0 +1,13 @@
+
+
+
+export default function Form() {
+   return (
+    <form action="" className="mt-4 flex flex-col">
+    <input type="email" name="email" id="email" placeholder="Enter your email" className="w-full rounded border border-white/50 bg-transparent px-3 py-2 text-white placeholder:text-white/50 md:max-w-[18rem]" />
+    <button type="submit" className="mt-4 w-full max-w-[14rem] rounded bg-white/30 px-14 py-2 text-center text-white">Subscribe</button>
+  </form>
+   )
+}
+
+m
